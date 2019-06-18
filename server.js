d5c513bfb11a531d7bec304c57d14510e15f7bba
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-06-18 15:33:58 -0600

    Added styling

---
+++ server.js
@@ -0,0 +1,21 @@
+//load things needed
+var express = require('express');
+var app = express();
+
+//set view engine to ejs
+app.set('view engine', 'ejs');
+
+//use res.render to load up an ejs view file
+
+//index page
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+//about page
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
